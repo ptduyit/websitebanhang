a47e77bcb94d6c946f2a109c861b6c3be0c87725
hungpro1760@gmail.com <hungpro1760@gmail.com>
2019-04-29 09:36:33 +0700

    Thêm controller  thống kê

---
+++ WebsiteBanHang/Controllers/StatisticController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+        private readonly SaleDBContext _context;
+        public StatisticController(SaleDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
